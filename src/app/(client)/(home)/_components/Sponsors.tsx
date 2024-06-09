44e43936ca2735a19fee94d48e7d4a176e1c7c10
jrtilak <iamjrtilak@gmail.com>
2024-06-09 20:33:11 +0545

    chore: Update dependencies and add react-fast-marquee package for sponsors section

---
+++ src/app/(client)/(home)/_components/Sponsors.tsx
@@ -0,0 +1,35 @@
+import { SPONSORS } from "@/mock-data/sponsors";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const Sponsors = () => {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center wrapper">
+      <h3 className="text-xl md:text-2xl font-bold">Trusted By</h3>
+      <Marquee
+        gradient={true}
+        gradientWidth={80}
+        autoFill={true}
+        pauseOnHover={true}
+        className="flex gap-12 items-center justify-center"
+      >
+        {SPONSORS.map((sponsor, i) => (
+          <Link href={sponsor.website ?? "#"} key={i} target="_blank">
+            <Image
+              src={sponsor.logo}
+              alt={sponsor.name}
+              height={60}
+              width={60}
+              quality={100}
+              className="ml-9 md:ml-20 object-contain object-center"
+            />
+          </Link>
+        ))}
+      </Marquee>
+    </div>
+  );
+};
+
+export default Sponsors;
--- src/app/(client)/(home)/page.tsx
+++ src/app/(client)/(home)/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./_components/Hero";
+import Sponsors from "./_components/Sponsors";
 
 const Home = () => {
   return (
     <div className="flex flex-col gap-4">
       <Hero />
+      <Sponsors />
     </div>
   );
 };
