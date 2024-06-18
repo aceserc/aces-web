0ad0f4f5733994e1ef7a44146aa3ccc8a13b3983
jrtilak <iamjrtilak@gmail.com>
2024-06-18 14:17:19 +0545

    chore: Remove unused code and update health check route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,8 @@
 import { NextResponse } from "next/server";
 
-// GET /api/route
 // Health check route
 export const GET = async () => {
   return NextResponse.json({
     message: "Hello World!",
-    from: "ACES",
   });
 };
