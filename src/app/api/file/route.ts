958bbd11ab37acfbd53deb69e9cf501a8ba598b5
jrtilak <iamjrtilak@gmail.com>
2024-06-18 14:31:49 +0545

    chore: Refactor file route and improve file upload and deletion handling

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -4,16 +4,10 @@ import { deleteImageFromCloudinary } from "@/helpers/delete-image";
 import { isAdmin } from "@/helpers/is-admin";
 import { RESPONSES } from "@/constants/response.constant";
 
-/**
- * Handles the POST request for uploading a file.
- *
- * @body file - The file to upload.
- * @body folder - The folder to upload the file to.
- *
- * @returns data - The uploaded file data.
- */
+// uploads file to cloudinary
 export const POST = async (req: NextRequest) => {
   try {
+    // Check if the user is an admin
     if (!(await isAdmin())) {
       return NextResponse.json(RESPONSES.UNAUTHORIZED_ACCESS, {
         status: RESPONSES.UNAUTHORIZED_ACCESS.status,
@@ -52,7 +46,7 @@ export const POST = async (req: NextRequest) => {
     return NextResponse.json(
       {
         ...RESPONSES.CREATED,
-        message: "File uploaded successfully",
+        message: "File uploaded successfully!",
         data,
       },
       {
@@ -66,34 +60,38 @@ export const POST = async (req: NextRequest) => {
   }
 };
 
-/**
- *  Handles the DELETE request for deleting a file.
- *
- * @body publicIds - The publicIds of the files to delete.
- *
- * @returns message - The response message.
- */
+// deletes files from cloudinary
 export const DELETE = async (req: NextRequest) => {
   try {
+    // Check if the user is an admin
     if (!(await isAdmin())) {
       return NextResponse.json(RESPONSES.UNAUTHORIZED_ACCESS, {
         status: RESPONSES.UNAUTHORIZED_ACCESS.status,
       });
     }
 
-    const { publicIds } = await req.json();
+    const { publicId } = await req.json();
 
-    if (!publicIds || !Array.isArray(publicIds)) {
+    if (!publicId) {
       return NextResponse.json(
         {
           ...RESPONSES.INSUFFICIENT_REQUEST_BODY,
-          message: "Please provide an array of publicIds to delete",
+          message: "Please provide one or more publicIds to delete",
         },
         {
           status: RESPONSES.INSUFFICIENT_REQUEST_BODY.status,
         }
       );
     }
+
+    let publicIds: string[] = [];
+
+    if (typeof publicId === "string") {
+      publicIds = [publicId];
+    } else {
+      publicIds = publicId;
+    }
+
     try {
       for (const publicId of publicIds) {
         if (!publicId) continue;
@@ -102,7 +100,7 @@ export const DELETE = async (req: NextRequest) => {
       return NextResponse.json(
         {
           ...RESPONSES.SUCCESS,
-          message: "Files deleted successfully",
+          message: "Files deleted successfully!",
         },
         {
           status: RESPONSES.SUCCESS.status,
@@ -112,7 +110,7 @@ export const DELETE = async (req: NextRequest) => {
       return NextResponse.json(
         {
           ...RESPONSES.SOMETHING_WENT_WRONG,
-          message: "Failed to delete files",
+          message: "Failed to delete files!",
         },
         {
           status: RESPONSES.SOMETHING_WENT_WRONG.status,
@@ -123,7 +121,7 @@ export const DELETE = async (req: NextRequest) => {
     return NextResponse.json(
       {
         ...RESPONSES.INTERNAL_SERVER_ERROR,
-        message: "Failed to delete files",
+        message: "Failed to delete files!",
       },
       {
         status: RESPONSES.INTERNAL_SERVER_ERROR.status,
