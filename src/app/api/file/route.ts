4c5b9d5d626d95d5661cae044851bbc08d8e51f8
jrtilak <iamjrtilak@gmail.com>
2024-06-23 19:13:54 +0545

    chore: Refactor file route to use middlewares for authentication, database connection, error handling, and response sending

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,91 +1,60 @@
 import { uploadFile } from "@/helpers/upload-file";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { deleteImageFromCloudinary } from "@/helpers/delete-image";
-import { isAdmin } from "@/helpers/is-admin";
-import { RESPONSES } from "@/constants/response.constant";
+import { applyMiddleware } from "@/middlewares/apply.middleware";
+import { isAdminMiddleware } from "@/middlewares/auth.middleware";
+import { connectToDBMiddleware } from "@/middlewares/db.middleware";
+import catchAsyncError from "@/middlewares/error-handler.middleware";
+import { sendNextResponse } from "@/middlewares/send-response";
 
 // uploads file to cloudinary
-export const POST = async (req: NextRequest) => {
-  try {
-    // Check if the user is an admin
-    if (!(await isAdmin())) {
-      return NextResponse.json(RESPONSES.UNAUTHORIZED_ACCESS, {
-        status: RESPONSES.UNAUTHORIZED_ACCESS.status,
-      });
-    }
-
+export const POST = applyMiddleware(
+  isAdminMiddleware,
+  connectToDBMiddleware,
+  catchAsyncError(async (req: NextRequest) => {
     const formData = await req.formData();
     const file = formData.get("file") as unknown as File;
     const folder = formData.get("folder") as string;
 
-    if (!file)
-      return NextResponse.json(
-        {
-          ...RESPONSES.INSUFFICIENT_REQUEST_BODY,
-          message: "Please provide a file to upload",
-        },
-        {
-          status: RESPONSES.INSUFFICIENT_REQUEST_BODY.status,
-        }
-      );
+    if (!file) {
+      return sendNextResponse({
+        status: 400,
+        message: "Please provide a file to upload",
+      });
+    }
 
     if (!folder) {
-      return NextResponse.json(
-        {
-          ...RESPONSES.INSUFFICIENT_REQUEST_BODY,
-          message: "Please provide a folder to upload the file",
-        },
-        {
-          status: RESPONSES.INSUFFICIENT_REQUEST_BODY.status,
-        }
-      );
+      return sendNextResponse({
+        status: 400,
+        message: "Please provide a folder to upload the file",
+      });
     }
 
     const data: any = await uploadFile(file, folder);
 
-    return NextResponse.json(
-      {
-        ...RESPONSES.CREATED,
-        message: "File uploaded successfully!",
-        data,
-      },
-      {
-        status: RESPONSES.CREATED.status,
-      }
-    );
-  } catch (error) {
-    return NextResponse.json(RESPONSES.INTERNAL_SERVER_ERROR, {
-      status: RESPONSES.INTERNAL_SERVER_ERROR.status,
+    return sendNextResponse({
+      status: 201,
+      message: "File uploaded successfully!",
+      data,
     });
-  }
-};
+  })
+);
 
 // deletes files from cloudinary
-export const DELETE = async (req: NextRequest) => {
-  try {
-    // Check if the user is an admin
-    if (!(await isAdmin())) {
-      return NextResponse.json(RESPONSES.UNAUTHORIZED_ACCESS, {
-        status: RESPONSES.UNAUTHORIZED_ACCESS.status,
-      });
-    }
-
+export const DELETE = applyMiddleware(
+  isAdminMiddleware,
+  connectToDBMiddleware,
+  catchAsyncError(async (req: NextRequest) => {
     const { publicId } = await req.json();
 
     if (!publicId) {
-      return NextResponse.json(
-        {
-          ...RESPONSES.INSUFFICIENT_REQUEST_BODY,
-          message: "Please provide one or more publicIds to delete",
-        },
-        {
-          status: RESPONSES.INSUFFICIENT_REQUEST_BODY.status,
-        }
-      );
+      return sendNextResponse({
+        status: 400,
+        message: "Please provide a publicId to delete the file",
+      });
     }
 
     let publicIds: string[] = [];
-
     if (typeof publicId === "string") {
       publicIds = [publicId];
     } else {
@@ -97,35 +66,15 @@ export const DELETE = async (req: NextRequest) => {
         if (!publicId) continue;
         await deleteImageFromCloudinary(publicId);
       }
-      return NextResponse.json(
-        {
-          ...RESPONSES.SUCCESS,
-          message: "Files deleted successfully!",
-        },
-        {
-          status: RESPONSES.SUCCESS.status,
-        }
-      );
+      return sendNextResponse({
+        status: 200,
+        message: "Files deleted successfully!",
+      });
     } catch (error) {
-      return NextResponse.json(
-        {
-          ...RESPONSES.SOMETHING_WENT_WRONG,
-          message: "Failed to delete files!",
-        },
-        {
-          status: RESPONSES.SOMETHING_WENT_WRONG.status,
-        }
-      );
-    }
-  } catch (error) {
-    return NextResponse.json(
-      {
-        ...RESPONSES.INTERNAL_SERVER_ERROR,
+      return sendNextResponse({
+        status: 500,
         message: "Failed to delete files!",
-      },
-      {
-        status: RESPONSES.INTERNAL_SERVER_ERROR.status,
-      }
-    );
-  }
-};
+      });
+    }
+  })
+);
