62f69afcaa3140b8eb9dba664739c93a624bfa2a
Tilak Thapa <tilakthapa9898@gmail.com>
2025-06-30 19:03:33 +0545

    out

---
+++ out/_next/static/1NFrBOdurlI1p0jqbQwXS/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fevents\u002F[id]","\u002Fnotices\u002F[slug]","\u002Ftraining-and-workshops\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ out/_next/static/chunks/app/(pages)/training-and-workshops/page-74b09b15f8fe0c91.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[0,526],{4054:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,3832,23)),Promise.resolve().then(n.t.bind(n,6874,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[874,832,441,684,358],()=>s(4054)),_N_E=e.O()}]);
