4193e5be1a256ec58c8d711afd810ae5281a33ae
jrtilak <tilakthapa9898@gmail.com>
2025-10-24 21:49:14 +0545

    feat: add gallery

---
+++ src/app/(pages)/gallery/page.tsx
@@ -0,0 +1,25 @@
+import { getCollection } from "@/db";
+import { Gallery } from "./_components";
+import { Gallery as GalleryType } from "@/db/types";
+import { Metadata } from "next";
+
+async function Page() {
+  const images = getCollection("gallery") as GalleryType[];
+  console.log(images);
+  return (
+    <Gallery
+      images={images.flatMap((i) =>
+        i.images.map((img) => ({
+          url: img,
+          tag: i.tag,
+        }))
+      )}
+    />
+  );
+}
+
+export default Page;
+
+export const metadata: Metadata = {
+  title: "Gallery | ACES",
+};
